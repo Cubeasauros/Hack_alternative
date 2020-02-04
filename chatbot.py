96bedf9fc6a93c9114424bc66e09b34bae30a5e0
Chinmay13mishra <53688567+Chinmay13mishra@users.noreply.github.com>
2020-02-04 22:00:53 +0530

    Update chatbot.py

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,7 @@
 import http_handle
 import sys
 
-send_data=http_handle.Wit('K5JLXDPZDAQUI26ZPBHJZUHMCNX37H5J')
+send_data=http_handle.Wit('GVTH5PSJSWFGPUMUXB2NCFFQN2JLZTZ2')
 out='ok'
 if len(sys.argv) != 2:
 	print('usage: python ' + sys.argv[0] + ' <wit-token>')
